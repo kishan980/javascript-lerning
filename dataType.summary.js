76620e7c7d83d3104eabeda971b869faff1e3a6a
kishan980 <kishany980>
2023-09-01 16:22:55 +0530

    data type and small summary

---
+++ dataType.summary.js
@@ -0,0 +1,41 @@
+//primitive
+
+//  7 type String, Number, Boolean , null ,undefined, symbol
+
+let name="kishan yadav";
+let numberData =123;
+let isLogged=true
+let numberAr=123.44
+let data;
+// console.log(data)
+// console.log(typeof name)
+// console.log(typeof numberAr)
+// console.log(typeof numberData)
+// console.log(typeof isLogged)
+// console.log(undefined)
+// console.log(null)
+
+// reference (Non primitive)
+
+// array, object, function
+
+const hereo=["kishan", "yadav", "kancha","yadav"]
+// console.log(typeof hereo)
+const obj={
+    name:"kishan",
+    age:21
+}
+// console.log(typeof obj)
+const myArray = function(){
+    console.log("hello kishan")
+}
+// console.log(typeof myArray)
+
+
+
+// stack premitive  - heap non premitive
+
+// stack store value and all premitive type datatype
+// heap store non premitive type all data type values
+
+
