574b93bb3bab01358003bfb9a2d05a8763a22a73
kishan980 <kishany980>
2023-09-01 14:33:32 +0530

    add value

---
+++ 03_convsrtionDataTyoe.js
@@ -0,0 +1,85 @@
+// let score =33;
+// console.log(typeof score)
+
+// let scroe ="33";
+// console.log(typeof scroe)
+
+// let scroe =null;
+// console.log(typeof scroe)
+
+// let scroe = undefined;
+// console.log(typeof scroe)
+
+// let score = "kishan"
+// let stringValue = Number(score)
+// console.log(typeof stringValue)
+// console.log(stringValue)
+
+
+// let isLogiiend ="kishan"
+// console.log(typeof isLogiiend)
+
+// let isLogiiend = true
+// console.log(typeof isLogiiend)
+
+// let isLogiiend =false
+// console.log(typeof isLogiiend)
+// console.log(isLogiiend)
+
+// let numberValue ="kishan"
+// let valueCheck =  Boolean(numberValue)
+// console.log(valueCheck)
+// console.log(typeof valueCheck)
+
+
+// let numberValue ="";
+// let checkValue =Boolean(numberValue)
+// console.log(numberValue)
+// console.log(typeof checkValue)
+
+
+// let name =44
+
+// let checkValue = String(name);
+// console.log(typeof checkValue)
+// console.log(checkValue)
+// console.log(typeof name)
+
+// let str1="hello";
+// let str2=" hitesh";
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1+"2")
+// console.log(1+"2")
+// console.log("1"+"2")
+
+// console.log(1+2+"3")
+
+// console.log((3+4)* 5%3)
+// console.log(true)
+// console.log(+true)
+// console.log(+false)
+
+// let numberAdd =1
+// let addValue =numberAdd++
+// console.log(numberAdd)
+// console.log(addValue)
+
+// console.log("2" >1)
+// console.log("02">1)
+// console.log(2 ==1)
+// console.log(2 !=1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+
+// console.log(undefined ==0)
+// console.log(undefined >0)
+// console.log(undefined<0)
+// console.log(undefined ===0)
+// console.log(undefined >=0)
+// console.log(undefined <=0)
