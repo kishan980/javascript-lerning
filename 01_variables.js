0526b3dec3c8c64b91e4bf7a3b45cdac19c2a51e
kishan980 <kishany980>
2023-09-01 11:39:22 +0530

    varibale

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId =12345678;
+let accountEmail ="kishany980@gmail.com"
+var accountPassword ="1234";
+
+accountCity ="Jaipur";
+
+// accountId=123
+accountEmail="kishan"
+// accountId=145263
+coountPassword=5263897
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountCity,accountPassword])
