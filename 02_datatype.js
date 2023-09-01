76cb7d7209e8eab1f537c306490582e860603334
kishan980 <kishany980>
2023-09-01 11:56:43 +0530

    data type first time

---
+++ 02_datatype.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let name="kishan"
+let age=18;
+let isLoggedIn=false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean
+// null
+// undfined
+// symbol =>unique
+// object
+
+
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof name)
+console.log(typeof age)
